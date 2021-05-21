424068912f44a533202ccccf62768b967fd21ab7
Nick Raimo <raimo.nick9@gmail.com>
2021-05-21 13:21:03 -0700

    sprint challenge

---
+++ src/Homepage.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import { useHistory } from 'react-router-dom'
+export default function Home() {
+    const history = useHistory()
+    const routeToOrder = () => {
+        console.log('Submit to go to order page')
+        history.push('/order-pizza')
+    }
+
+    return (
+        <div className='order-pizza'>
+            <img
+                className='pizza-img'
+                src='D:\Lambda\Web43\Unit2-3\web-sprint-challenge-single-page-applications\Assets\Pizza.jpg'
+                alt='Fresh Pizza'
+            />
+            <button
+                onClick={routeToOrder}
+                className='pizza-button'
+            >
+                Order Here!
+            </button>
+        </div>
+    )
+}
